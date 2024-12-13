e4e462bfe07b0e73ed9e2e937ac6694eac817295
PedroDearmas <pedrodearmas1234@gmail.com>
2024-12-13 01:12:45 -0300

    cadastro usuario com senha encriptada salvando no mongodb

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import publicRoutes from './routes/public.js'
 
 const app = express()
 
-app.listen(3000, () => console.log("Servidor rodando!"))
+app.use(express.json())
+
+app.use('/', publicRoutes)
+
+app.listen(3000, () => console.log("Servidor rodando!!🚀🚀"))
+
+
+//Pedro
+//pedrodearmas2003144
+//mongodb+srv://Pedro:pedrodearmas2003144@users.g4jwe.mongodb.net/?retryWrites=true&w=majority&appName=Users
