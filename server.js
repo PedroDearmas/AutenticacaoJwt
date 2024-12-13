26f6738682dd0f0a75aa80d0970fbeeb75151840
PedroDearmas <pedrodearmas1234@gmail.com>
2024-12-13 19:59:55 -0300

    usuario faz login e recebe token jwt

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
+import privateRoutes from './routes/private.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/', publicRoutes)
+app.use('/', privateRoutes)
 
 app.listen(3000, () => console.log("Servidor rodando!!🚀🚀"))
-
-
-//Pedro
-//pedrodearmas2003144
-//mongodb+srv://Pedro:pedrodearmas2003144@users.g4jwe.mongodb.net/?retryWrites=true&w=majority&appName=Users
