c980307302931daf1982ab933227ba3e2d4cc8a8
PedroDearmas <pedrodearmas1234@gmail.com>
2024-12-16 22:30:26 -0300

    configurando projeto para deploy no render

--- backend/server.js
+++ backend/server.js
@@ -13,3 +13,8 @@ app.use('/', publicRoutes)
 app.use('/', auth, privateRoutes)
 
 app.listen(3000, () => console.log("Servidor rodando!!🚀🚀"))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
